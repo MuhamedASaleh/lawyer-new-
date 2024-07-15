b7fe2769e735df8dff2146f15b23944147a77fe9
beshoynasryz <beshoynasry0@gmail.com>
2024-07-15 09:41:15 +0300

    deploy1

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -1,17 +1,9 @@
 const News = require('../models/newsModel');
-const Admin = require('../models/adminModel');
 
 // Create a news
 const createNews = async (req, res) => {
     try {
-        const { description, image, adminID } = req.body;
-        
-        // Verify admin exists
-        const admin = await Admin.findByPk(adminID);
-        if (!admin) {
-            return res.status(404).json({ message: 'Admin not found' });
-        }
-
+        const { description, image } = req.body;
         const news = await News.create({ description, image });
         res.status(201).json(news);
     } catch (error) {
@@ -22,28 +14,18 @@ const createNews = async (req, res) => {
 // Get all news
 const getAllNews = async (req, res) => {
     try {
-        const news = await News.findAll({
-            include: [{
-                model: Admin,
-                attributes: ['phoneNumber']
-            }]
-        });
+        const news = await News.findAll();
         res.status(200).json(news);
     } catch (error) {
         res.status(500).json({ message: error.message });
-    }
+    } 
 };
 
 // Get news by ID
 const getNewsById = async (req, res) => {
     try {
         const { id } = req.params;
-        const news = await News.findByPk(id, {
-            include: [{
-                model: Admin,
-                attributes: ['phoneNumber']
-            }]
-        });
+        const news = await News.findByPk(id);
         if (!news) {
             return res.status(404).json({ message: 'News not found' });
         }
@@ -70,7 +52,7 @@ const updateNews = async (req, res) => {
 
         res.status(200).json(news);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        res.status{500}.json({ message: error.message });
     }
 };
 
@@ -98,3 +80,105 @@ module.exports = {
     updateNews,
     deleteNews
 };
+
+
+// const News = require('../models/newsModel');
+// const Admin = require('../models/adminModel');
+
+// // Create a news
+// const createNews = async (req, res) => {
+//     try {
+//         const { description, image, adminID } = req.body;
+        
+//         // Verify admin exists
+//         const admin = await Admin.findByPk(adminID);
+//         if (!admin) {
+//             return res.status(404).json({ message: 'Admin not found' });
+//         }
+
+//         const news = await News.create({ description, image });
+//         res.status(201).json(news);
+//     } catch (error) {
+//         res.status(500).json({ message: error.message });
+//     }
+// };
+
+// // Get all news
+// const getAllNews = async (req, res) => {
+//     try {
+//         const news = await News.findAll({
+//             include: [{
+//                 model: Admin,
+//                 attributes: ['phoneNumber']
+//             }]
+//         });
+//         res.status(200).json(news);
+//     } catch (error) {
+//         res.status(500).json({ message: error.message });
+//     }
+// };
+
+// // Get news by ID
+// const getNewsById = async (req, res) => {
+//     try {
+//         const { id } = req.params;
+//         const news = await News.findByPk(id, {
+//             include: [{
+//                 model: Admin,
+//                 attributes: ['phoneNumber']
+//             }]
+//         });
+//         if (!news) {
+//             return res.status(404).json({ message: 'News not found' });
+//         }
+//         res.status(200).json(news);
+//     } catch (error) {
+//         res.status(500).json({ message: error.message });
+//     }
+// };
+
+// // Update a news
+// const updateNews = async (req, res) => {
+//     try {
+//         const { id } = req.params;
+//         const { description, image } = req.body;
+
+//         const news = await News.findByPk(id);
+//         if (!news) {
+//             return res.status(404).json({ message: 'News not found' });
+//         }
+
+//         news.description = description;
+//         news.image = image;
+//         await news.save();
+
+//         res.status(200).json(news);
+//     } catch (error) {
+//         res.status(500).json({ message: error.message });
+//     }
+// };
+
+// // Delete a news
+// const deleteNews = async (req, res) => {
+//     try {
+//         const { id } = req.params;
+
+//         const news = await News.findByPk(id);
+//         if (!news) {
+//             return res.status(404).json({ message: 'News not found' });
+//         }
+
+//         await news.destroy();
+//         res.status(200).json({ message: 'News deleted' });
+//     } catch (error) {
+//         res.status(500).json({ message: error.message });
+//     }
+// };
+
+// module.exports = {
+//     createNews,
+//     getAllNews,
+//     getNewsById,
+//     updateNews,
+//     deleteNews
+// };
