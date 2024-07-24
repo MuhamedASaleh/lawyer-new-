f091be511137e5a12c457b5f819a866ed403b86d
beshoynasryz <beshoynasry0@gmail.com>
2024-07-24 15:57:02 +0300

    currnet ans completed cases

--- routes/caseRoutes.js
+++ routes/caseRoutes.js
@@ -1,22 +1,38 @@
-const router = require('express').Router()
+const router = require('express').Router();
+const { 
+  filterCurrentCase, 
+  filterCurrentCaseAdmin,
+  filterCompletedCases, 
+  filterCompletedCasesAdmin,
+  createCase, 
+  getCaseDetails, 
+  updateCaseStatus, 
+  updateCustomerFiles 
+} = require('../controllers/caseController');
+const { Auth, AuthorizeRole } = require('../middleware/auth');
 
-const { filterCurrentCase, filterCurrentCaseAdmin ,createCase,getCaseDetails,updateCaseStatus,updateCustomerFiles} = require('../controllers/caseController')
-
-const { Auth, AuthorizeRole } = require('../middleware/auth')
 // Route to create a new case
-router.post('/create', Auth, AuthorizeRole('lawyer'),createCase);
+router.post('/create', Auth, AuthorizeRole('lawyer'), createCase);
 
 // Route to get case details
 router.get('/:caseId/details', getCaseDetails);
 
-// Route to update case status to accepted(for customers only)
-router.put('/:caseId/updateStatus', caseController.updateCaseStatus);
-
-// Route to decline and delete a case
-// router.delete('/:caseId/declineAndDelete', Auth, caseController.declineAndDeleteCase);;
+// Route to update case status to accepted (for customers only)
+router.put('/:caseId/updateStatus', updateCaseStatus);
 
 // Route to update customer files (for customers only)
-router.put('/:caseId/updateCustomerFiles',caseController.updateCustomerFiles)
+router.put('/:caseId/updateCustomerFiles', updateCustomerFiles);
 
+// Route to filter current cases
+router.get('/filterCurrentCase', Auth,AuthorizeRole('lawyer','customer'), filterCurrentCase);
+
+// Route to filter current cases for admin
+router.get('/filterCurrentCaseAdmin', Auth, AuthorizeRole('admin'), filterCurrentCaseAdmin);
+
+// Route to filter completed cases (win or lose) for the current user
+router.get('/filterCompletedCases', Auth,AuthorizeRole('lawyer','customer') ,filterCompletedCases);
+
+// Route to filter completed cases (win or lose) for admin
+router.get('/filterCompletedCasesAdmin', Auth, AuthorizeRole('admin'), filterCompletedCasesAdmin);
 
 module.exports = router;
