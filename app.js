5786fbca8a80ea7af6464ecf408ccfe5f30e92c6
muhamedASaleh <muhamedsaleh233@gmail.com>
2024-07-29 18:06:08 +0300

    socket io is fired

--- app.js
+++ app.js
@@ -103,86 +103,54 @@
 //   // Add additional event listeners here
 // });
 
-
-
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const path = require('path');
 
 const app = express();
-require("dotenv").config({ path: ".env" });
-const sequelize = require('./config/dbConfig');
-const cors = require("cors");
-const bodyParser = require("body-parser");
-
-app.use(express.static(path.join(__dirname, 'public')));
-let port = process.env.PORT || 5050;
-
-// Create an HTTP server
 const server = http.createServer(app);
-
-// Integrate Socket.io with the server
 const io = socketIo(server);
 
-app.get('/', (req, res) => {
-  res.send('home');
-});
+app.use(express.static('public'));
 
-// db connection
-sequelize.sync({ force: false })
-  .then(() => {
-    console.log('Database synced');
-    server.listen(port, () => {
-      console.log(`Server is running on http://localhost:${port}`);
-    });
-  })
-  .catch(err => {
-    console.error('Error syncing database:', err);
-  });
-
-app.use(cors({ origin: "*" }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const port = process.env.PORT || 5050;
 
 // Serve the test HTML file
 app.get('/test', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'test.html'));
 });
 
-// Socket.io logic
 io.on('connection', (socket) => {
-  console.log(`New client connected: ${socket.id}`);
-
-  socket.on('disconnect', () => {
-    console.log(`Client disconnected: ${socket.id}`);
-    socket.broadcast.emit('callEnded');
-  });
-
-  socket.on('setUsername', (username) => {
-    socket.username = username;
-    io.emit('updateUsers', getUsers());
-  });
+  console.log('New client connected: ' + socket.id);
 
   socket.on('callUser', (data) => {
-    io.to(data.userToCall).emit('callUser', { signal: data.signalData, from: socket.id, name: socket.username });
+    io.to(data.userToCall).emit('callUser', {
+      signal: data.signalData,
+      from: socket.id,
+    });
   });
 
   socket.on('answerCall', (data) => {
-    io.to(data.to).emit('callAccepted', { signal: data.signal, from: socket.id });
+    io.to(data.to).emit('callAccepted', {
+      signal: data.signal,
+      from: socket.id,
+    });
   });
 
   socket.on('iceCandidate', (data) => {
-    io.to(data.to).emit('iceCandidate', { candidate: data.candidate });
+    io.to(data.to).emit('iceCandidate', {
+      candidate: data.candidate,
+    });
   });
 
-  function getUsers() {
-    const clients = io.sockets.sockets;
-    const users = {};
-    for (const id in clients) {
-      users[id] = clients[id].username || 'Anonymous';
-    }
-    return users;
-  }
+  socket.on('disconnect', () => {
+    console.log('Client disconnected: ' + socket.id);
+    // Notify other clients about disconnection
+    io.emit('userDisconnected', socket.id);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
