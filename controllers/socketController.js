c16aa2804b93bb3b79e0a4642b4f0a16b8efcf6c
muhamedASaleh <muhamedsaleh233@gmail.com>
2024-07-30 13:35:43 +0300

    current case with filter 1

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -1,42 +1,5 @@
 
 // controllers/socketController.js
-// exports.handleSocketConnection = function (io) {
-//     io.on('connection', (socket) => {
-//         console.log('A user connected');
-//         // Notify others of the new user
-//         socket.broadcast.emit('new-user', { userId: socket.id });
-
-//         // Handle incoming offer
-//         socket.on('offer', (data) => {
-//             // Notify the target user of the incoming call
-//             socket.to(data.target).emit('incomingCall', {
-//                 sdp: data.sdp,
-//                 caller: socket.id,
-//             });
-//         });
-
-//         // Handle incoming answer
-//         socket.on('answer', (data) => {
-//             socket.to(data.target).emit('answer', {
-//                 sdp: data.sdp,
-//                 callee: socket.id,
-//             });
-//         });
-
-//         // Handle incoming ICE candidate
-//         socket.on('candidate', (data) => {
-//             socket.to(data.target).emit('candidate', { candidate: data.candidate });
-//         });
-
-//         // Handle user disconnection
-//         socket.on('disconnect', () => {
-//             console.log('A user disconnected');
-//         });
-//     });
-// };
-//////////////////////////////
-
-// controllers/socketController.js
 exports.handleSocketConnection = function (io) {
     io.on('connection', (socket) => {
         console.log('A user connected');
