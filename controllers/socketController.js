b47b7c2dd787aec709a0b35260a147779bdd26c5
muhamedASaleh <muhamedsaleh233@gmail.com>
2024-07-30 12:55:30 +0300

    current case with filter 1

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -1,61 +1,27 @@
-// // controllers/socketController.js
-// exports.handleSocketConnection = function (io) {
-//     io.on('connection', (socket) => {
-//         console.log('A user connected');
-//         // Notify others of the new user
-//         socket.broadcast.emit('new-user', { userId: socket.id });
-//         socket.on('offer', (data) => {
-//             socket.to(data.target).emit('offer', {
-//                 sdp: data.sdp,
-//                 caller: socket.id,
-//             });
-//         });
-//         socket.on('answer', (data) => {
-//             socket.to(data.target).emit('answer', {
-//                 sdp: data.sdp,
-//                 callee: socket.id,
-//             });
-//         });
-//         socket.on('candidate', (data) => {
-//             socket.to(data.target).emit('candidate', { candidate: data.candidate });
-//         });
-//         socket.on('disconnect', () => {
-//             console.log('A user disconnected');
-//         });
-//     });
-// };
-///////////////////////
 // controllers/socketController.js
 exports.handleSocketConnection = function (io) {
     io.on('connection', (socket) => {
         console.log('A user connected');
-
+        // Notify others of the new user
+        socket.broadcast.emit('new-user', { userId: socket.id });
         socket.on('offer', (data) => {
             socket.to(data.target).emit('offer', {
                 sdp: data.sdp,
                 caller: socket.id,
             });
-            // Notify the target user about the incoming call
-            socket.to(data.target).emit('callNotification', { caller: socket.id });
         });
-
         socket.on('answer', (data) => {
             socket.to(data.target).emit('answer', {
                 sdp: data.sdp,
                 callee: socket.id,
             });
         });
-
         socket.on('candidate', (data) => {
             socket.to(data.target).emit('candidate', { candidate: data.candidate });
         });
-
-        socket.on('endCall', () => {
-            socket.broadcast.emit('endCallNotification');
-        });
-
         socket.on('disconnect', () => {
             console.log('A user disconnected');
         });
     });
 };
+
