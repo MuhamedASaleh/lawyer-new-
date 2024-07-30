9ad9db896ad68e376d6b63753ff159875ebddd1a
muhamedASaleh <muhamedsaleh233@gmail.com>
2024-07-30 13:02:35 +0300

    current case with filter 1

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -1,30 +1,43 @@
-// controllers/socketController.js
+
+// // controllers/socketController.js
 // exports.handleSocketConnection = function (io) {
 //     io.on('connection', (socket) => {
 //         console.log('A user connected');
 //         // Notify others of the new user
 //         socket.broadcast.emit('new-user', { userId: socket.id });
+
+//         // Handle incoming offer
 //         socket.on('offer', (data) => {
-//             socket.to(data.target).emit('offer', {
+//             // Notify the target user of the incoming call
+//             socket.to(data.target).emit('incomingCall', {
 //                 sdp: data.sdp,
 //                 caller: socket.id,
 //             });
 //         });
+
+//         // Handle incoming answer
 //         socket.on('answer', (data) => {
 //             socket.to(data.target).emit('answer', {
 //                 sdp: data.sdp,
 //                 callee: socket.id,
 //             });
 //         });
+
+//         // Handle incoming ICE candidate
 //         socket.on('candidate', (data) => {
 //             socket.to(data.target).emit('candidate', { candidate: data.candidate });
 //         });
+
+//         // Handle user disconnection
 //         socket.on('disconnect', () => {
 //             console.log('A user disconnected');
 //         });
 //     });
 // };
-/////////////////////////////////
+
+
+
+//////////////////////////
 // controllers/socketController.js
 exports.handleSocketConnection = function (io) {
     io.on('connection', (socket) => {
@@ -32,16 +45,13 @@ exports.handleSocketConnection = function (io) {
         // Notify others of the new user
         socket.broadcast.emit('new-user', { userId: socket.id });
 
-        // Handle incoming offer
         socket.on('offer', (data) => {
-            // Notify the target user of the incoming call
             socket.to(data.target).emit('incomingCall', {
                 sdp: data.sdp,
                 caller: socket.id,
             });
         });
 
-        // Handle incoming answer
         socket.on('answer', (data) => {
             socket.to(data.target).emit('answer', {
                 sdp: data.sdp,
@@ -49,15 +59,17 @@ exports.handleSocketConnection = function (io) {
             });
         });
 
-        // Handle incoming ICE candidate
         socket.on('candidate', (data) => {
             socket.to(data.target).emit('candidate', { candidate: data.candidate });
         });
 
-        // Handle user disconnection
+        // Notify the other user when a call ends
+        socket.on('endCall', () => {
+            socket.broadcast.emit('endCallNotification');
+        });
+
         socket.on('disconnect', () => {
             console.log('A user disconnected');
         });
     });
 };
-
