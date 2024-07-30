daface6d0f6637c1fb7499cd2a8eff9927c11989
muhamedASaleh <muhamedsaleh233@gmail.com>
2024-07-30 12:59:09 +0300

    current case with filter 1

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -1,24 +1,60 @@
 // controllers/socketController.js
+// exports.handleSocketConnection = function (io) {
+//     io.on('connection', (socket) => {
+//         console.log('A user connected');
+//         // Notify others of the new user
+//         socket.broadcast.emit('new-user', { userId: socket.id });
+//         socket.on('offer', (data) => {
+//             socket.to(data.target).emit('offer', {
+//                 sdp: data.sdp,
+//                 caller: socket.id,
+//             });
+//         });
+//         socket.on('answer', (data) => {
+//             socket.to(data.target).emit('answer', {
+//                 sdp: data.sdp,
+//                 callee: socket.id,
+//             });
+//         });
+//         socket.on('candidate', (data) => {
+//             socket.to(data.target).emit('candidate', { candidate: data.candidate });
+//         });
+//         socket.on('disconnect', () => {
+//             console.log('A user disconnected');
+//         });
+//     });
+// };
+/////////////////////////////////
+// controllers/socketController.js
 exports.handleSocketConnection = function (io) {
     io.on('connection', (socket) => {
         console.log('A user connected');
         // Notify others of the new user
         socket.broadcast.emit('new-user', { userId: socket.id });
+
+        // Handle incoming offer
         socket.on('offer', (data) => {
-            socket.to(data.target).emit('offer', {
+            // Notify the target user of the incoming call
+            socket.to(data.target).emit('incomingCall', {
                 sdp: data.sdp,
                 caller: socket.id,
             });
         });
+
+        // Handle incoming answer
         socket.on('answer', (data) => {
             socket.to(data.target).emit('answer', {
                 sdp: data.sdp,
                 callee: socket.id,
             });
         });
+
+        // Handle incoming ICE candidate
         socket.on('candidate', (data) => {
             socket.to(data.target).emit('candidate', { candidate: data.candidate });
         });
+
+        // Handle user disconnection
         socket.on('disconnect', () => {
             console.log('A user disconnected');
         });
