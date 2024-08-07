9ed9a2e14464475c36274c3c72433fea3019e2f9
muhamedASaleh <muhamedsaleh233@gmail.com>
2024-08-07 13:36:33 +0300

    solve order proplem

--- controllers/socket/pubsub/video.js
+++ controllers/socket/pubsub/video.js
@@ -1,5 +1,7 @@
-let clientCaller = { userId: null, socketID: null };
-let clientCallee = { userId: null, socketID: null };
+const { createRoom, addUserBySocketId, deleteRoomById } = require('../controller/Room');
+
+let clientCaller = { userId: null, socketID: null, roomId: null };
+let clientCallee = { userId: null, socketID: null, roomId: null };lientCallee = { userId: null, socketID: null };
 
 const video = (socket, io) => {
   socket.on('joinRoom', (userId) => {
@@ -46,3 +48,74 @@ const video = (socket, io) => {
 };
 
 module.exports = video;
+
+
+
+
+// const { createRoom, addUserBySocketId, deleteRoomById } = require('../controller/Room');
+
+// let clientCaller = { userId: null, socketID: null, roomId: null };
+// let clientCallee = { userId: null, socketID: null, roomId: null };
+
+// const video = (socket, io) => {
+//   socket.on('userEnterRoom', async (userId) => {
+//     if (!clientCaller.userId) {
+//       clientCaller = { userId, socketID: socket.id, roomId: null };
+//       io.to(socket.id).emit('userWaiting', `Hello, waiting for other user...`);
+//     } else {
+//       clientCallee = { userId, socketID: socket.id, roomId: null };
+
+//       // Create a new room
+//       const roomId = await createRoom();
+//       if (roomId) {
+//         clientCaller.roomId = roomId;
+//         clientCallee.roomId = roomId;
+
+//         await addUserBySocketId(roomId, clientCaller.socketID);
+//         await addUserBySocketId(roomId, clientCallee.socketID);
+
+//         io.to(clientCaller.socketID).emit('roomReady', `Connecting you to a video chat`);
+//         io.to(clientCallee.socketID).emit('roomReady', `Connecting you to a video chat`);
+        
+//         // Notify both users about the room creation
+//         io.to(clientCaller.socketID).emit('notification', { message: 'Room created', roomId });
+//         io.to(clientCallee.socketID).emit('notification', { message: 'Room created', roomId });
+//       }
+//     }
+//   });
+
+//   socket.on('videoChatOffer', ({ sdp }) => {
+//     if (socket.id === clientCaller.socketID) {
+//       io.to(clientCallee.socketID).emit('getVideoChatOffer', sdp);
+//     }
+//   });
+
+//   socket.on('videoChatAnswer', ({ sdp }) => {
+//     io.to(clientCaller.socketID).emit('getVideoChatAnswer', sdp);
+//   });
+
+//   socket.on('candidate', ({ candidate }) => {
+//     if (socket.id === clientCaller.socketID) {
+//       io.to(clientCallee.socketID).emit('getCandidate', candidate);
+//     } else {
+//       io.to(clientCaller.socketID).emit('getCandidate', candidate);
+//     }
+//   });
+
+//   socket.on('disconnect', async () => {
+//     if (socket.id === clientCaller.socketID || socket.id === clientCallee.socketID) {
+//       const roomId = clientCaller.roomId || clientCallee.roomId;
+//       if (roomId) {
+//         await deleteRoomById(roomId);
+        
+//         // Notify both users about the room destruction
+//         io.to(clientCaller.socketID).emit('notification', { message: 'Room destroyed', roomId });
+//         io.to(clientCallee.socketID).emit('notification', { message: 'Room destroyed', roomId });
+//       }
+//       clientCaller = { userId: null, socketID: null, roomId: null };
+//       clientCallee = { userId: null, socketID: null, roomId: null };
+//     }
+//   });
+// };
+
+// module.exports = video;
